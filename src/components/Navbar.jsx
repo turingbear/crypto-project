bb3657c7ef6cd43a04e48c53b3ee5d905afb623a
nsect <nsecttheory@gmail.com>
2022-10-16 15:46:51 -0700

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import { Link, useNavigate } from 'react-router-dom';
+import ThemeToggle from './ThemeToggle';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { useState } from 'react';
+import {UserAuth} from '../context/AuthContext';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const {user, logOut} = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSignout = async (e) => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch {
+      console.log(e.message);
+    }
+  };
+
+  const handleNav = () => setNav(!nav);
+
+  return (
+    <div className="rounded-div flex items-center justify-between h-20 font-bold">
+      <Link to="/">
+        <h1 className="text-2xl">Cryptobase</h1>
+      </Link>
+      <div className="hidden md:block">
+        <ThemeToggle />
+      </div>
+
+      {user?.email ? (
+        <div>
+          <Link to="/account" className="p-4">
+            Account
+          </Link>
+          <button onClick={handleSignout}>Sign Out</button>
+        </div>
+      ) : (
+        <div className="hidden md:block">
+          <Link className="p-4 hover:text-accent" to="/signin">
+            Sign In
+          </Link>
+          <Link
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+            to="/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
+
+      {/* Menu Icon */}
+      <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      {/* Mobile Menu */}
+      <div
+        className={
+          nav
+            ? 'md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10'
+            : 'fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300'
+        }
+      >
+        <ul className="w-full p-4">
+          <li onClick={handleNav} className="border-b py-6">
+            <Link to="/">Home</Link>
+          </li>
+          <li onClick={handleNav} className="border-b py-6">
+            <Link to="/account">Account</Link>
+          </li>
+          <li className="py-6">
+            <ThemeToggle /> 
+          </li>
+        </ul>
+        <div className="flex flex-col w-full p-4">
+          <Link to="/signin">
+            <button onClick={handleNav} className="w-full my-2 p-3 bg-primary text-primary border border-secondary rounded-2xl shadow-xl">
+              Sign In
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button onClick={handleNav} className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import CoinSearch from '../components/CoinSearch'
+import Trending from '../components/Trending'
+
+const Home = ({coins}) => {
+  return (
+    <div>
+      <CoinSearch coins={coins}/>
+      <Trending />
+    </div>
+  )
+}
+export default Home
